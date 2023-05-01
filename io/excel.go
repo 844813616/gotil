d75d335b4a6282eaa3fbe372eeaaae212e50d87d
weiguanqun <844813616@qq.com>
2023-05-01 19:06:04 +0800

    support update cell by areas

--- io/excel.go
+++ io/excel.go
@@ -39,7 +39,7 @@ func WriteExcel(filePath string, content [][]string) error {
 				log.Println(err)
 				continue
 			}
-			if err := file.SetCellValue(defaultSheetName, cellName, value); err != nil {
+			if err := file.SetCellDefault(defaultSheetName, cellName, value); err != nil {
 				return err
 			}
 		}
@@ -50,20 +50,150 @@ func WriteExcel(filePath string, content [][]string) error {
 	return nil
 }
 
-func UpdateExcel(filePath string, content map[string]string) error {
-	return nil
-}
-
 func ReadExcel(filePath string, sheetName string) (content [][]string, err error) {
 	file, err := excelize.OpenFile(filePath)
 	if err != nil {
 		return nil, err
 	}
 	defer func() {
-		// Close the spreadsheet.
 		if err := file.Close(); err != nil {
 			log.Println(err)
 		}
 	}()
 	return file.GetRows(sheetName)
+}
+
+func UpdateExcel(file *excelize.File, sheetName string, cellNames []string, calAreas []Area, processor func(in [][][]string) string) error {
+	index, err := file.GetSheetIndex(sheetName)
+	if err != nil {
+		return err
+	}
+	file.SetActiveSheet(index)
+	for _, cellName := range cellNames {
+		var in [][][]string
+		for _, calAreas := range calAreas {
+			cellNameArray := calAreas.ToCellNameArray(cellName)
+			in = append(in, cellNameArray)
+		}
+		in = getAreasCellValue(file, sheetName, in)
+		out := processor(in)
+		if err := file.SetCellDefault(sheetName, cellName, out); err != nil {
+			log.Println(err)
+		}
+	}
+	if err := file.Save(); err != nil {
+		return err
+	}
+	return nil
+}
+
+type Value interface {
+	Get(int) int
+}
+
+type Absolute int
+
+func (a Absolute) Get(_ int) int {
+	return int(a)
+}
+
+type Relative int
+
+func (r Relative) Get(relative int) int {
+	return int(r) + relative
+}
+
+type Coordinate struct {
+	row Value
+	col Value
+}
+
+func (c Coordinate) GetCellName(relative string) string {
+	col, row, err := excelize.CellNameToCoordinates(relative)
+	if err != nil {
+		return ""
+	}
+	cellName, err := excelize.CoordinatesToCellName(c.col.Get(col), c.row.Get(row))
+	if err != nil {
+		return ""
+	}
+	return cellName
+}
+
+type Area struct {
+	leftUp      Coordinate
+	rightBottom Coordinate
+}
+
+func (a Area) ToCellNameList(relative string) []string {
+	left, right, up, bottom, err := a.GetBoundary(relative)
+	if err != nil {
+		return nil
+	}
+	var ret []string
+	for col := left; col <= right; col++ {
+		for row := up; row <= bottom; row++ {
+			cellName, err := excelize.CoordinatesToCellName(col, row)
+			if err != nil {
+				return nil
+			}
+			ret = append(ret, cellName)
+		}
+	}
+	return ret
+}
+
+func (a Area) ToCellNameArray(relative string) [][]string {
+	left, right, up, bottom, err := a.GetBoundary(relative)
+	if err != nil {
+		return nil
+	}
+	var ret [][]string
+	for row := up; row <= bottom; row++ {
+		var rowRet []string
+		for col := left; col <= right; col++ {
+			cellName, err := excelize.CoordinatesToCellName(col, row)
+			if err != nil {
+				return nil
+			}
+			rowRet = append(rowRet, cellName)
+		}
+		ret = append(ret, rowRet)
+	}
+	return ret
+}
+
+func (a Area) GetBoundary(relative string) (left, right, up, bottom int, err error) {
+	leftUpCellName := a.leftUp.GetCellName(relative)
+	left, up, err = excelize.CellNameToCoordinates(leftUpCellName)
+	if err != nil {
+		return 0, 0, 0, 0, err
+	}
+	rightBottomCellName := a.rightBottom.GetCellName(relative)
+	right, bottom, err = excelize.CellNameToCoordinates(rightBottomCellName)
+	if err != nil {
+		return 0, 0, 0, 0, err
+	}
+	return left, right, up, bottom, nil
+}
+
+func getAreasCellValue(file *excelize.File, sheetName string, cellNames [][][]string) [][][]string {
+	var ret [][][]string
+	for _, area := range cellNames {
+		var retArea [][]string
+		for _, row := range area {
+			var retRow []string
+			for _, col := range row {
+				cellName := col
+				value, err := file.GetCellValue(sheetName, cellName)
+				if err != nil {
+					return nil
+				}
+				retRow = append(retRow, value)
+			}
+			retArea = append(retArea, retRow)
+		}
+		ret = append(ret, retArea)
+	}
+	return ret
 }
