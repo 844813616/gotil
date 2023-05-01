777a34571897950afae8fd8c145015a7f540e1ac
weiguanqun <844813616@qq.com>
2023-05-01 16:13:33 +0800

    extract IsNotExist func

--- io/path.go
+++ io/path.go
@@ -7,11 +7,16 @@ import (
 
 func Mkdir(filePath string) error {
 	dir := path.Dir(filePath)
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
+	if IsNotExist(dir) {
 		err := os.MkdirAll(dir, os.ModePerm)
 		if err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func IsNotExist(path string) bool {
+	_, err := os.Stat(path)
+	return os.IsNotExist(err)
 }
