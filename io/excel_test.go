3a9590551b236d47be681391231662493294b303
weiguanqun <844813616@qq.com>
2023-05-01 19:06:04 +0800

    support update cell by areas

--- io/excel_test.go
+++ io/excel_test.go
@@ -1,6 +1,7 @@
 package io
 
 import (
+	"github.com/xuri/excelize/v2"
 	"log"
 	"testing"
 )
@@ -24,5 +25,40 @@ func TestReadExcel(t *testing.T) {
 			print(cell + " ")
 		}
 		println()
+	}
+}
+
+func TestUpdateExcel(t *testing.T) {
+	var m = make(map[string]bool)
+	processor := func(in [][][]string) string {
+		for _, a := range in {
+			for _, r := range a {
+				for _, c := range r {
+					if m[c] == false {
+						m[c] = true
+						return c
+					}
+				}
+			}
+		}
+		return ""
+	}
+	file, err := excelize.OpenFile(testExcelPath)
+	if err != nil {
+		return
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+	err = UpdateExcel(file, defaultSheetName,
+		[]string{"D1", "D2"},
+		[]Area{
+			{Coordinate{Absolute(1), Absolute(1)}, Coordinate{Absolute(3), Absolute(1)}},
+			{Coordinate{Absolute(1), Absolute(2)}, Coordinate{Absolute(1), Absolute(3)}},
+		}, processor)
+	if err != nil {
+		log.Println(err)
 	}
 }
